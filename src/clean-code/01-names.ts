c639e3407bc636db0e064f58bab54cc6fc1606af
ariana0505 <penaariana075@gmail.com>
2025-03-23 20:54:19 -0500

    first commit

---
+++ src/clean-code/01-names.ts
@@ -0,0 +1,47 @@
+(() => {
+
+    // Ejemplo
+    // Archivos a evaluar - files to evaluate
+    const fs = [
+        { id: 1, flagged: false },
+        { id: 2, flagged: false },
+        { id: 3, flagged: true },
+        { id: 4, flagged: false },
+        { id: 5, flagged: false },
+        { id: 7, flagged: true },
+    ]; 
+    
+    // Archivos marcados para borrar - files to delete
+    const arhivos = fs.map( file => file.flagged);
+
+    
+    // Mejor
+    class User { };
+    interface User { };
+
+
+    // Todo: Tarea
+        
+    // día de hoy - today
+    const toDay = new Date();
+    
+    // días transcurridos - elapsed time in days
+    const elapsedTimeIndays: number = 23;
+    
+    // número de archivos en un directorio - number of files in directory
+    const FilesIndirectory = 33;
+    
+    // primer nombre - first name
+    const firstName  = 'Fernando';
+    
+    // primer apellido - last name
+    const lastName = 'Herrera';
+
+    // días desde la última modificación - days since modification
+    const daysModification = 12;
+    
+    // cantidad máxima de clases por estudiante - max classes per student
+    const maxClassStudent = 6;
+
+
+})();
